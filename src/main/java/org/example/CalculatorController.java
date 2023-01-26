1203f28361b3fa6c9f6e305e94682e8f168ac24e
Ahmad Barutchi <ahmad.barutchi@gmail.com>
2023-01-26 07:27:24 +0100

    initial commit

---
+++ src/main/java/org/example/CalculatorController.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+    @GetMapping("/calculate")
+    public double calculate(@RequestParam double num1, @RequestParam double num2, @RequestParam String operation) {
+        switch (operation) {
+            case "add":
+                return num1 + num2;
+            case "subtract":
+                return num1 - num2;
+            case "multiply":
+                return num1 * num2;
+            case "divide":
+                return num1 / num2;
+            default:
+                return 0;
+        }
+    }
+}
